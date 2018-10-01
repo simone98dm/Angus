14fdf04f3f56409309e3368c210be542136a5d9e
simone98dm <simonedalmas1@gmail.com>
2018-10-01 15:52:18 +0200

    add new layout for sidebar

--- src/api/config.js
+++ src/api/config.js
@@ -5,8 +5,8 @@
  */
 module.exports = {
   secret: "e50e9ea800f93efd7db9b7a8d6f69af5e045ebdd804519e23527ea263d75fc93",
-  hostname: "192.168.1.160",
+  hostname: "localhost",
   username: "piedpiper",
   password: "PiedPiper2018",
-  db_name: "db_piedpiper"
+  db_name: "piedpiper"
 };
