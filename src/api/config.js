54609dc397c482b1f8f95bcded105b7e263848b9
simone98dm <simonedalmas1@gmail.com>
2018-10-09 10:39:26 +0200

    add user role example

--- src/api/config.js
+++ src/api/config.js
@@ -6,7 +6,7 @@
 module.exports = {
   secret: "e50e9ea800f93efd7db9b7a8d6f69af5e045ebdd804519e23527ea263d75fc93",
   hostname: "localhost",
-  username: "piedpiper",
+  username: "db_piedpiper",
   password: "PiedPiper2018",
   db_name: "piedpiper"
 };
