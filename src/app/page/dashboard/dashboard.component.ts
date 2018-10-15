0944e9d376752a5439dd638030f93ef74b8c4d69
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2018-10-15 14:22:58 +0200

    Now the graphs will spawn even whithout data and they will refresh if new data is added

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {SummaryDTO} from '../../models/Summary';
 import {ArchiveService} from '../../services/archive.service';
 import {ProfileDTO} from '../../models/Profile';
@@ -12,6 +12,9 @@ import { RetriveChartService } from '../../services/retrive-chart.service';
 })
 export class DashboardComponent implements OnInit {
 
+  @ViewChild('energy') energyChart;
+  @ViewChild('water') waterChart;
+  @ViewChild('uptime') uptimeChart;
   summaryCardItems: SummaryDTO[] = [
     {title: 'Temperatura', text: 'Description1', value: '1234', icon: '', style: 'primary'},
     {title: 'Numero Giri', text: 'Description2', value: '4567', icon: '', style: 'danger'},
@@ -20,7 +23,10 @@ export class DashboardComponent implements OnInit {
 
   energySummaryChart: any = {
     chartType: 'ColumnChart',
-    dataTable: [],
+    dataTable: [
+      ['Consumi Elettrici', 'Settimana',  'Attuale'],
+      ['Energia', 0, 0]
+    ],
     options: {
       title: 'Consumi Elettrici',
       height: 623
@@ -29,7 +35,10 @@ export class DashboardComponent implements OnInit {
 
   waterSummaryChart: any = {
     chartType: 'ColumnChart',
-    dataTable: [],
+    dataTable: [
+      ['Consumi Acqua', 'Settimana',  'Attuale'],
+      ['Acqua', 0, 0]
+    ],
     options: {
       title: 'Consumi Acqua',
       height: 623
@@ -38,7 +47,10 @@ export class DashboardComponent implements OnInit {
 
   uptimeSummaryChart: any = {
     chartType: 'ColumnChart',
-    dataTable: [],
+    dataTable: [
+      ['Uptime', 'Settimana',  'Attuale'],
+      ['Attività', 0, 0]
+    ],
     options: {
       title: 'Uptime',
       height: 623
@@ -59,16 +71,20 @@ export class DashboardComponent implements OnInit {
       this.energySummaryChart.dataTable.length = 0;
       this.energySummaryChart.dataTable.push(['Consumi Elettrici', 'Settimana',  'Attuale']);
       this.energySummaryChart.dataTable.push(['Energia', data.energy_Average, data.energy_Instant]);
+      this.energyChart.redraw();
       //-------------------------
       this.waterSummaryChart = Object.create(this.waterSummaryChart);
       this.waterSummaryChart.dataTable.length = 0;
       this.waterSummaryChart.dataTable.push(['Consumi Acqua', 'Settimana',  'Attuale']);
       this.waterSummaryChart.dataTable.push(['Acqua', data.water_Average, data.water_Instant]);
+      this.waterChart.redraw();
       //-------------------------
       this.uptimeSummaryChart = Object.create(this.uptimeSummaryChart);
       this.uptimeSummaryChart.dataTable.length = 0;
       this.uptimeSummaryChart.dataTable.push(['Uptime', 'Settimana',  'Attuale']);
       this.uptimeSummaryChart.dataTable.push(['Attività', data.uptime_Average, data.uptime_Instant]);
+      this.uptimeChart.redraw();
+
     });
   }
 }
