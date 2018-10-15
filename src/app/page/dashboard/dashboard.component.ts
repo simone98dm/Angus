c654acbd4ced2f19e4932458565e26dd03b7377d
simone98dm <simonedalmas1@gmail.com>
2018-10-15 15:05:41 +0200

    add refresh rate obj and event emitter

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -2,8 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {SummaryDTO} from '../../models/Summary';
 import {ArchiveService} from '../../services/archive.service';
 import {ProfileDTO} from '../../models/Profile';
-import { RetriveChartService } from '../../services/retrive-chart.service';
-
+import {RetriveChartService} from '../../services/retrive-chart.service';
+import {RefreshRateDTO} from '../../models/RefreshRate';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,6 +11,8 @@ import { RetriveChartService } from '../../services/retrive-chart.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+
+  refreshRate: RefreshRateDTO;
 
   summaryCardItems: SummaryDTO[] = [
     {title: 'Temperatura', text: 'Description1', value: '1234', icon: '', style: 'primary'},
@@ -25,7 +27,7 @@ export class DashboardComponent implements OnInit {
       title: 'Consumi Elettrici',
       height: 623
     }
-  }
+  };
 
   waterSummaryChart: any = {
     chartType: 'ColumnChart',
@@ -34,7 +36,7 @@ export class DashboardComponent implements OnInit {
       title: 'Consumi Acqua',
       height: 623
     }
-  }
+  };
 
   uptimeSummaryChart: any = {
     chartType: 'ColumnChart',
@@ -43,7 +45,7 @@ export class DashboardComponent implements OnInit {
       title: 'Uptime',
       height: 623
     }
-  }
+  };
 
   loggedUser: ProfileDTO = this.archive.getProfile();
 
@@ -53,22 +55,26 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
     this.socket.reclaimSupervisorHome();
     this.socket.getSupervisorHome()
-    .subscribe((data: any) => {
-      console.log(data);
-      this.energySummaryChart = Object.create(this.energySummaryChart);
-      this.energySummaryChart.dataTable.length = 0;
-      this.energySummaryChart.dataTable.push(['Consumi Elettrici', 'Settimana',  'Attuale']);
-      this.energySummaryChart.dataTable.push(['Energia', data.energy_Average, data.energy_Instant]);
-      //-------------------------
-      this.waterSummaryChart = Object.create(this.waterSummaryChart);
-      this.waterSummaryChart.dataTable.length = 0;
-      this.waterSummaryChart.dataTable.push(['Consumi Acqua', 'Settimana',  'Attuale']);
-      this.waterSummaryChart.dataTable.push(['Acqua', data.water_Average, data.water_Instant]);
-      //-------------------------
-      this.uptimeSummaryChart = Object.create(this.uptimeSummaryChart);
-      this.uptimeSummaryChart.dataTable.length = 0;
-      this.uptimeSummaryChart.dataTable.push(['Uptime', 'Settimana',  'Attuale']);
-      this.uptimeSummaryChart.dataTable.push(['Attività', data.uptime_Average, data.uptime_Instant]);
-    });
+      .subscribe((data: any) => {
+        console.log(data);
+        this.energySummaryChart = Object.create(this.energySummaryChart);
+        this.energySummaryChart.dataTable.length = 0;
+        this.energySummaryChart.dataTable.push(['Consumi Elettrici', 'Settimana', 'Attuale']);
+        this.energySummaryChart.dataTable.push(['Energia', data.energy_Average, data.energy_Instant]);
+        //-------------------------
+        this.waterSummaryChart = Object.create(this.waterSummaryChart);
+        this.waterSummaryChart.dataTable.length = 0;
+        this.waterSummaryChart.dataTable.push(['Consumi Acqua', 'Settimana', 'Attuale']);
+        this.waterSummaryChart.dataTable.push(['Acqua', data.water_Average, data.water_Instant]);
+        //-------------------------
+        this.uptimeSummaryChart = Object.create(this.uptimeSummaryChart);
+        this.uptimeSummaryChart.dataTable.length = 0;
+        this.uptimeSummaryChart.dataTable.push(['Uptime', 'Settimana', 'Attuale']);
+        this.uptimeSummaryChart.dataTable.push(['Attività', data.uptime_Average, data.uptime_Instant]);
+      });
+  }
+
+  setRefreshrate(refresh: number) {
+    this.refreshRate = refresh;
   }
 }
