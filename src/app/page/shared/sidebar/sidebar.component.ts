97f8bfcc10b08005affa59688470157b971db5c2
simone98dm <simonedalmas1@gmail.com>
2018-10-15 13:28:44 +0200

    add subsscribe for area

--- src/app/page/shared/sidebar/sidebar.component.ts
+++ src/app/page/shared/sidebar/sidebar.component.ts
@@ -20,7 +20,7 @@ export class SidebarComponent implements OnInit {
     this.updateAreas();
   }
 
-  updateAreas(){
+  updateAreas() {
     this.factory.getAreas()
       .subscribe((response: IFactoryStructure) => {
         this.areaList = [];
