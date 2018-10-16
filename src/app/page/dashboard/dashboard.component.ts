5fd1c469b8063bc60f771737eccb239f4559c6e7
simone98dm <simonedalmas1@gmail.com>
2018-10-16 14:53:17 +0200

    fix migrate select

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {SummaryDTO} from '../../models/Summary';
 import {ArchiveService} from '../../services/archive.service';
 import {ProfileDTO} from '../../models/Profile';
@@ -6,7 +6,6 @@ import {IFactoryStructure} from '../shared/sidebar/sidebar.component';
 import {RetriveDataService} from '../../services/retrive-data.service';
 
 import {RetriveChartService} from '../../services/retrive-chart.service';
-import {RefreshRateDTO} from '../../models/RefreshRate';
 
 @Component({
   selector: 'app-dashboard',
@@ -18,7 +17,7 @@ export class DashboardComponent implements OnInit {
   @ViewChild('energy') energyChart;
   @ViewChild('water') waterChart;
   @ViewChild('uptime') uptimeChart;
-  refreshRate: RefreshRateDTO;
+  refreshRate: string;
 
   summaryCardItems: SummaryDTO[] = [
     {title: 'Temperatura', text: 'Description1', value: '1234', icon: '', style: 'primary'},
@@ -26,26 +25,18 @@ export class DashboardComponent implements OnInit {
     {title: 'Livello massimo acqua', text: 'Description3', value: '89', icon: '', style: 'success'},
   ];
 
-  @Output()
-  refreshGraphRate = new EventEmitter();
-
   refreshOptions = [
     {id: 0, name: 'Istantaneo'},
     {id: 1, name: '1 Min'},
     {id: 2, name: '1 Ora'},
     {id: 3, name: '1 Giorno'},
     {id: 4, name: '1 Sett'},
-    {id: 5, name: '1 Mese'},
-    {id: 6, name: '1 anno'}
   ];
-
-  refreshRate: string;
-
 
   energySummaryChart: any = {
     chartType: 'ColumnChart',
     dataTable: [
-      ['Consumi Elettrici', 'Settimana',  'Attuale'],
+      ['Consumi Elettrici', 'Settimana', 'Attuale'],
       ['Energia', 0, 0]
     ],
     options: {
@@ -57,7 +48,7 @@ export class DashboardComponent implements OnInit {
   waterSummaryChart: any = {
     chartType: 'ColumnChart',
     dataTable: [
-      ['Consumi Acqua', 'Settimana',  'Attuale'],
+      ['Consumi Acqua', 'Settimana', 'Attuale'],
       ['Acqua', 0, 0]
     ],
     options: {
@@ -69,7 +60,7 @@ export class DashboardComponent implements OnInit {
   uptimeSummaryChart: any = {
     chartType: 'ColumnChart',
     dataTable: [
-      ['Uptime', 'Settimana',  'Attuale'],
+      ['Uptime', 'Settimana', 'Attuale'],
       ['Attività', 0, 0]
     ],
     options: {
@@ -89,34 +80,34 @@ export class DashboardComponent implements OnInit {
     }
     this.socket.reclaimSupervisorHome();
     this.socket.getSupervisorHome()
-    .subscribe((data: any) => {
-      console.log(data);
-      this.energySummaryChart = Object.create(this.energySummaryChart);
-      this.energySummaryChart.dataTable.length = 0;
-      this.energySummaryChart.dataTable.push(['Consumi Elettrici', 'Settimana',  'Attuale']);
-      this.energySummaryChart.dataTable.push(['Energia', data.energy_Average, data.energy_Instant]);
-      this.energyChart.redraw();
-      //-------------------------
-      this.waterSummaryChart = Object.create(this.waterSummaryChart);
-      this.waterSummaryChart.dataTable.length = 0;
-      this.waterSummaryChart.dataTable.push(['Consumi Acqua', 'Settimana',  'Attuale']);
-      this.waterSummaryChart.dataTable.push(['Acqua', data.water_Average, data.water_Instant]);
-      this.waterChart.redraw();
-      //-------------------------
-      this.uptimeSummaryChart = Object.create(this.uptimeSummaryChart);
-      this.uptimeSummaryChart.dataTable.length = 0;
-      this.uptimeSummaryChart.dataTable.push(['Uptime', 'Settimana',  'Attuale']);
-      this.uptimeSummaryChart.dataTable.push(['Attività', data.uptime_Average, data.uptime_Instant]);
-      this.uptimeChart.redraw();
+      .subscribe((data: any) => {
+        console.log(data);
+        this.energySummaryChart = Object.create(this.energySummaryChart);
+        this.energySummaryChart.dataTable.length = 0;
+        this.energySummaryChart.dataTable.push(['Consumi Elettrici', 'Settimana', 'Attuale']);
+        this.energySummaryChart.dataTable.push(['Energia', data.energy_Average, data.energy_Instant]);
+        this.energyChart.redraw();
+        //-------------------------
+        this.waterSummaryChart = Object.create(this.waterSummaryChart);
+        this.waterSummaryChart.dataTable.length = 0;
+        this.waterSummaryChart.dataTable.push(['Consumi Acqua', 'Settimana', 'Attuale']);
+        this.waterSummaryChart.dataTable.push(['Acqua', data.water_Average, data.water_Instant]);
+        this.waterChart.redraw();
+        //-------------------------
+        this.uptimeSummaryChart = Object.create(this.uptimeSummaryChart);
+        this.uptimeSummaryChart.dataTable.length = 0;
+        this.uptimeSummaryChart.dataTable.push(['Uptime', 'Settimana', 'Attuale']);
+        this.uptimeSummaryChart.dataTable.push(['Attività', data.uptime_Average, data.uptime_Instant]);
+        this.uptimeChart.redraw();
 
-    });
+      });
   }
 
   setRefreshRate(refresh: number) {
     this.refreshRate = refresh;
   }
 
-  updateAreas(){
+  updateAreas() {
     this.factory.getAreas()
       .subscribe((response: IFactoryStructure) => {
         let areaList = [];
