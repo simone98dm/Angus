63e4f773ea17e8fd35c7685b97ddf07864148722
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2018-10-17 15:58:20 +0200

    Fix Supervisor visualization and chart refresh

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -104,6 +104,7 @@ export class DashboardComponent implements OnInit {
       });
     this.socket.getManutentorHome()
       .subscribe((data: IManutentorData) => {
+        console.log(data);
         this.manutentor_data = data;
       });
   }
