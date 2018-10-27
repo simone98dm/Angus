ba84428d0d3d0272de2870ef24fe00f339dc61b0
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2018-10-27 15:50:20 +0200

    Data retrieval for graphs splitted in more parts, add aero graphs

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -1,112 +1,253 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {SummaryDTO} from '../../models/Summary';
-import {ArchiveService} from '../../services/archive.service';
-import {ProfileDTO} from '../../models/Profile';
-import {IFactoryStructure} from '../shared/sidebar/sidebar.component';
-import {RetriveDataService} from '../../services/retrive-data.service';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { SummaryDTO } from "../../models/Summary";
+import { ArchiveService } from "../../services/archive.service";
+import { ProfileDTO } from "../../models/Profile";
+import { IFactoryStructure } from "../shared/sidebar/sidebar.component";
+import { RetriveDataService } from "../../services/retrive-data.service";
 
-import {RetriveChartService} from '../../services/retrive-chart.service';
+import { RetriveChartService } from "../../services/retrive-chart.service";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.css"]
 })
 export class DashboardComponent implements OnInit {
+  @ViewChild("energy")
+  energyChart;
+  @ViewChild("water")
+  waterChart;
+  @ViewChild("uptime")
+  uptimeChart;
+  @ViewChild("energy_grouped")
+  energyAero;
+  @ViewChild("water_grouped")
+  waterAero;
 
-  @ViewChild('energy') energyChart;
-  @ViewChild('water') waterChart;
-  @ViewChild('uptime') uptimeChart;
   refreshRate: number;
 
+  water_Grouped: any = {
+    chartType: "PieChart",
+    dataTable: [["Consumi Acqua", "Giorno"], ["No data", 50]],
+    options: {
+      title: "Acqua",
+      slices: {
+        0: {
+          offset: 0.3
+        },
+        1: {
+          offset: 0.2
+        }
+      }
+    }
+  };
+
+  energy_Grouped: any = {
+    chartType: "PieChart",
+    dataTable: [["Consumi Acqua", "Giorno"], ["No data", 100]],
+    options: {
+      title: "Energia",
+      slices: {
+        0: {
+          offset: 0.3
+        },
+        1: {
+          offset: 0.2
+        }
+      }
+    }
+  };
+
   summaryCardItems: SummaryDTO[] = [
-    {title: 'Temperatura', text: 'Description1', value: '1234', icon: '', style: 'primary'},
-    {title: 'Numero Giri', text: 'Description2', value: '4567', icon: '', style: 'danger'},
-    {title: 'Livello massimo acqua', text: 'Description3', value: '89', icon: '', style: 'success'},
+    {
+      title: "Temperatura",
+      text: "Description1",
+      value: "1234",
+      icon: "",
+      style: "primary"
+    },
+    {
+      title: "Numero Giri",
+      text: "Description2",
+      value: "4567",
+      icon: "",
+      style: "danger"
+    },
+    {
+      title: "Livello massimo acqua",
+      text: "Description3",
+      value: "89",
+      icon: "",
+      style: "success"
+    }
   ];
 
   refreshOptions = [
-    {id: 0, name: 'Istantaneo'},
-    {id: 1, name: '1 Min'},
-    {id: 2, name: '1 Ora'},
-    {id: 3, name: '1 Giorno'},
-    {id: 4, name: '1 Sett'},
+    { id: 0, name: "Istantaneo" },
+    { id: 1, name: "1 Min" },
+    { id: 2, name: "1 Ora" },
+    { id: 3, name: "1 Giorno" },
+    { id: 4, name: "1 Sett" }
   ];
 
-  manutentor_data: IManutentorData = null;
+  manutentor_data: IManutentorData = {
+    pretreatment: {
+      finisher: {
+        max: 0,
+        min: 0
+      },
+      primer: {
+        max: 0,
+        min: 0
+      },
+      pretreatment: {
+        max: 0,
+        min: 0
+      }
+    },
+    prewashing: {
+      temp: 0,
+      revolutionxminute: 0,
+      water_level: {
+        max: 0,
+        min: 0
+      }
+    },
+    washing: {
+      temp: 0,
+      revolutionxminute: 0,
+      water_level: {
+        max: 0,
+        min: 0
+      }
+    },
+    drying: {
+      temp: 0,
+      revolutionxminute: 0
+    },
+    storage: {
+      engine_one: {
+        revolutionxminute: 0
+      },
+      engine_two: {
+        revolutionxminute: 0
+      }
+    }
+  };
 
   energySummaryChart: any = {
-    chartType: 'ColumnChart',
+    chartType: "ColumnChart",
     dataTable: [
-      ['Consumi Elettrici', 'Settimana', 'Attuale'],
-      ['Energia', 0, 0]
+      ["Consumi Elettrici", "Settimana", "Attuale"],
+      ["Energia", 0, 0]
     ],
     options: {
-      title: 'Consumi Elettrici',
+      title: "Consumi Elettrici",
       height: 623
     }
   };
 
   waterSummaryChart: any = {
-    chartType: 'ColumnChart',
-    dataTable: [
-      ['Consumi Acqua', 'Settimana', 'Attuale'],
-      ['Acqua', 0, 0]
-    ],
+    chartType: "ColumnChart",
+    dataTable: [["Consumi Acqua", "Settimana", "Attuale"], ["Acqua", 0, 0]],
     options: {
-      title: 'Consumi Acqua',
+      title: "Consumi Acqua",
       height: 623
     }
   };
 
   uptimeSummaryChart: any = {
-    chartType: 'ColumnChart',
-    dataTable: [
-      ['Uptime', 'Settimana', 'Attuale'],
-      ['Attività', 0, 0]
-    ],
+    chartType: "ColumnChart",
+    dataTable: [["Uptime", "Settimana", "Attuale"], ["Attività", 0, 0]],
     options: {
-      title: 'Uptime',
+      title: "Uptime",
       height: 623
     }
   };
   public loggedUser: ProfileDTO = this.archive.getProfile();
 
-  constructor(private archive: ArchiveService, private factory: RetriveDataService, private socket: RetriveChartService) {
-  }
+  constructor(
+    private archive: ArchiveService,
+    private factory: RetriveDataService,
+    private socket: RetriveChartService
+  ) {}
 
   ngOnInit() {
     if (this.archive.getAreas() == null) {
       this.updateAreas();
     }
     this.socket.reclaimHomeData(this.loggedUser.grade);
-    this.socket.getSupervisorHome()
-      .subscribe((data: any) => {
-        console.log(data);
-        this.energySummaryChart = Object.create(this.energySummaryChart);
-        this.energySummaryChart.dataTable.length = 0;
-        this.energySummaryChart.dataTable.push(['Consumi Elettrici', 'Settimana', 'Attuale']);
-        this.energySummaryChart.dataTable.push(['Energia', data.energy_Average, data.energy_Instant]);
-        this.energyChart.redraw();
-        // -------------------------
-        this.waterSummaryChart = Object.create(this.waterSummaryChart);
-        this.waterSummaryChart.dataTable.length = 0;
-        this.waterSummaryChart.dataTable.push(['Consumi Acqua', 'Settimana', 'Attuale']);
-        this.waterSummaryChart.dataTable.push(['Acqua', data.water_Average, data.water_Instant]);
-        this.waterChart.redraw();
-        // -------------------------
-        this.uptimeSummaryChart = Object.create(this.uptimeSummaryChart);
-        this.uptimeSummaryChart.dataTable.length = 0;
-        this.uptimeSummaryChart.dataTable.push(['Uptime', 'Settimana', 'Attuale']);
-        this.uptimeSummaryChart.dataTable.push(['Attività', data.uptime_Average, data.uptime_Instant]);
-        this.uptimeChart.redraw();
+    this.socket.getSupervisorHome_Energy().subscribe((data: any) => {
+      console.log(data);
+      this.energySummaryChart = Object.create(this.energySummaryChart);
+      this.energySummaryChart.dataTable.length = 0;
+      this.energySummaryChart.dataTable.push([
+        "Consumi Elettrici",
+        "Settimana",
+        "Attuale"
+      ]);
+      this.energySummaryChart.dataTable.push([
+        "Energia",
+        data.energy_Average,
+        data.energy_Instant
+      ]);
+      this.energyChart.redraw();
+    });
 
+    this.socket.getSupervisorHome_Uptime().subscribe((data: any) => {
+      // -------------------------
+      this.uptimeSummaryChart = Object.create(this.uptimeSummaryChart);
+      this.uptimeSummaryChart.dataTable.length = 0;
+      this.uptimeSummaryChart.dataTable.push([
+        "Uptime",
+        "Settimana",
+        "Attuale"
+      ]);
+      this.uptimeSummaryChart.dataTable.push([
+        "Attività",
+        data.uptime_Average,
+        data.uptime_Instant
+      ]);
+      this.uptimeChart.redraw();
+    });
+
+    this.socket.getSupervisorHome_Water().subscribe((data: any) => {
+      // -------------------------
+      this.waterSummaryChart = Object.create(this.waterSummaryChart);
+      this.waterSummaryChart.dataTable.length = 0;
+      this.waterSummaryChart.dataTable.push([
+        "Consumi Acqua",
+        "Settimana",
+        "Attuale"
+      ]);
+      this.waterSummaryChart.dataTable.push([
+        "Acqua",
+        data.water_Average,
+        data.water_Instant
+      ]);
+      this.waterChart.redraw();
+    });
+
+    this.socket.getManutentorHome().subscribe((data: IManutentorData) => {
+      console.log(data);
+      this.manutentor_data = data;
+    });
+
+    this.socket.getSupervisorHome_AeroGraphs().subscribe((data: any) => {
+      console.log(data);
+      this.water_Grouped = Object.create(this.water_Grouped);
+
+      this.water_Grouped.dataTable.lenght = 0;
+      this.water_Grouped.dataTable.push(["Consumi Acqua", "Giorno"]);
+      data.energy.forEach(element => {
+        this.water_Grouped.dataTable.push([
+          element.machine_name,
+          element.value
+        ]);
       });
-    this.socket.getManutentorHome()
-      .subscribe((data: IManutentorData) => {
-        console.log(data);
-        this.manutentor_data = data;
-      });
+
+      this.waterAero.redraw();
+    });
   }
 
   setRefreshRate(refresh: number) {
@@ -114,17 +255,16 @@ export class DashboardComponent implements OnInit {
   }
 
   updateAreas() {
-    this.factory.getAreas()
-      .subscribe((response: IFactoryStructure) => {
-        let areaList = [];
-        for (let item of response.result) {
-          areaList.push({
-            id: item.pLineId,
-            name: item.pLineName
-          });
-        }
-        this.archive.setAreas(areaList);
-      });
+    this.factory.getAreas().subscribe((response: IFactoryStructure) => {
+      let areaList = [];
+      for (let item of response.result) {
+        areaList.push({
+          id: item.pLineId,
+          name: item.pLineName
+        });
+      }
+      this.archive.setAreas(areaList);
+    });
   }
 }
 
--- src/app/services/retrive-chart.service.ts
+++ src/app/services/retrive-chart.service.ts
@@ -9,16 +9,28 @@ export class RetriveChartService {
 
   reclaimHomeData(auth_id) {
     this.socket.emit('home_run', auth_id);
-  }  
+  }
 
-  getSupervisorHome() {
-    return this.socket.fromEvent('supervisor_data_home').map(data => data);
+  getSupervisorHome_AeroGraphs() {
+    return this.socket.fromEvent('supervisor_data_aero_graphs').map(data => data);
+  }
+
+  getSupervisorHome_Energy() {
+    return this.socket.fromEvent('supervisor_data_home_energy');
+  }
+
+  getSupervisorHome_Water() {
+    return this.socket.fromEvent('supervisor_data_home_water');
+  }
+
+  getSupervisorHome_Uptime() {
+    return this.socket.fromEvent('supervisor_data_home_uptime');
   }
 
   getManutentorHome() {
     return this.socket.fromEvent('manutentor_data_home').map(data => data);
   }
-  
+
   getInstantEnergyDrainBySensor() {
     return this.socket.fromEvent('instant_energy').map(data => data);
   }
